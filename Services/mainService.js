b2b917132c508542adc0670cb91bbb6f4de0cfcf
ruben69695 <ruben.arre6@gmail.com>
2018-03-04 19:32:59 +0100

    Añado callback solo en addrule para probar

--- Services/mainService.js
+++ Services/mainService.js
@@ -5,60 +5,76 @@ var io = require('socket.io')(server);
 var sem = require('semaphore')(1);
 var InfoMessage = require("./../Classes/InfoMessage");
 var RuleLog = require("./../Classes/RuleLog");
+var MongoClient = require('mongodb').MongoClient;
+var nomdb="Firewalldb";
+var urlConnexio="mongodb://localhost:27017/";
 const _PORT = 3000;
 
 server.listen(_PORT, function() {
     console.log("Socket running on http://*:%s", _PORT);
     console.log("Fecha de ejecución del servidor -> " + new Date().toISOString());
+
+    /*var tempRule = new RuleLog("ASIX_SSH", "Apertura del puerto SSH", "", 22, "Crear", 
+    "in", "allow", "tcp", "Ruben Arrebola");
+
+    var numError = MongoInsertRule(tempRule); 
+    
+    console.log(numError);
+    
+    */
+
+    var result = MongoGetRules("2018-02-25T08:19:25.712Z");
+    
+    console.log(result);
+
 });
 
 io.sockets.on('connection', function(socket) {
-
-    var thisClient;
 
     console.log("Un cliente con IP %s , ha establecido conexión con el socket", socket.conn.remoteAddress);
 
     // PETICIONES A ATENDER POR PARTE DEL CLIENTE
     socket.on('addRule', function (json) {
-        enterToSemaphore("dbMuttable", "addRule", json);
+        enterToSemaphore("dbMuttable", "addRule", json, socket);
     });
 
     socket.on('modifyRule', function(json) {
-        enterToSemaphore("dbMuttable", "modifyRule", json);
+        enterToSemaphore("dbMuttable", "modifyRule", json, socket);
     });
 
     socket.on('deleteRule', function(json) {
-        enterToSemaphore("dbMuttable", "deleteRule", json);
+        enterToSemaphore("dbMuttable", "deleteRule", json, socket);
     });
 
     socket.on('enableRule', function(json) {
-        enterToSemaphore("dbMuttable", "enableRule", json);
+        enterToSemaphore("dbMuttable", "enableRule", json, socket);
     });
 
     socket.on('disableRule', function(json) {
-        enterToSemaphore("dbMuttable", "disableRule", json);
+        enterToSemaphore("dbMuttable", "disableRule", json, socket);
     });
 
     socket.on('getCurrentList', function (json) {
-        enterToSemaphore("dbImmutable", "getCurrentList", json);
+        enterToSemaphore("dbImmutable", "getCurrentList", json, socket);
     });
 
+});
 
-    /**
+/**
      * Función que controla con un semaforo los accesos a la base de datos
      * @param {string} dbAttackType Tipo de ataque contra la bbdd
      * @param {string} operation Elegir operacion
      * @param {string} json Información a pasarle a la función en formato json u otro formato
      */
-    function enterToSemaphore(dbAtackType, operation, json)
+    function enterToSemaphore(dbAtackType, operation, json, socket)
     {
         sem.take(1, function() {
             switch(dbAtackType) {
                 case "dbMuttable":
-                    flowMuttableOperations(operation, json);
+                    flowMuttableOperations(operation, json, socket);
                 break;
                 case "dbImmutable":
-                    flowImmutableOperations(operation, json);
+                    flowImmutableOperations(operation, json, socket);
                 break;
             }
             sem.leave(1);
@@ -70,14 +86,14 @@ io.sockets.on('connection', function(socket) {
      * @param {string} operation Tipo de operacion muttable sobre la base de datos
      * @param {string} json Información en formato JSON
      */
-    function flowMuttableOperations(operation, json)
+    function flowMuttableOperations(operation, json, socket)
     {
         var result = JSON.parse(json);  // Deserializamos el JSON y creamos el objeto correcto
         var error = 0;                  // Default todo correcto
         var message;                    // Objeto de mensaje
         
         // Instanciamos una nueva regla con los datos correspondientes
-        var newRule = newRule = new RuleLog (       
+        var newRule = new RuleLog (       
             result.Name, result.Description, result.Path, result.Port, 
             result.Operation, result.InOut, result.Permission, result.Protocol, result.Author
         );
@@ -85,30 +101,41 @@ io.sockets.on('connection', function(socket) {
         if(operation == "addRule")
         {
             // A la descripción le concatenamos un espacio y la versión
-            newRule.Description = newRule.Description + " " + newRule.Version;         
-            message = createRule(newRule);
+            newRule.Description = newRule.Description + " " + newRule.Version;
+            newRule.Operation = "crear";         
+            //message = MongoInsertRule(newRule);
+            Mongo(function() {
+                message = MongoInsertRule(newRule);
+            });
         }
         else if(operation == "modifyRule")
         {
             // Crear un registro de eliminación para la regla pasada por JSON y luego crearla como nueva
-            message = deleteRule(newRule);
+            newRule.Operation = "eliminar";
+            message = MongoInsertRule(newRule);
             if(!message.Erno)
-                message = createRule(newRule);
+            {
+                newRule.Operation = "create";
+                message = MongoInsertRule(newRule);
+            }
         }
         else if(operation == "deleteRule")
         {
             // Crear un registro de eliminación para la regla pasada por JSON
-            message = deleteRule(newRule);
+            newRule.Operation = "eliminar";
+            message = MongoInsertRule(newRule);
         }
         else if(operation == "enableRule")
         {
             // Crar registro para habilitar la regla pasada por JSON
-            message = enableRule(newRule);
+            newRule.Operation = "habilitar";
+            message = MongoInsertRule(newRule);
         }
         else if(operation == "disableRule")
         {
             // Crear un registro para deshabilitar la regla pasada por JSON
-            message = disableRule(newRule);
+            newRule.Operation = "deshabilitar";
+            message = MongoInsertRule(newRule);
         }
 
         // Notificamos el resultado de la operación al cliente que ha solicitado realizar el cambio
@@ -161,84 +188,78 @@ io.sockets.on('connection', function(socket) {
         if(error == 0)
         {
             // Si ha ido bien hacemos un broadcast a todos los sockets
-            message = new InfoMessage(false, "Todo correcto, se ha insertado la nueva regla en base de datos", newRule);
+            message = new InfoMessage(false, "Todo correcto, se ha insertado la nueva regla en base de datos", rule);
         }
-        else
+        else if(error == 2)
         {
             // Si no avisamos al cliente socket que ha ido mal y el mensaje de error
-            message = new InfoMessage(true, "Error al insertar el registro en base de datos", newRule);
+            message = new InfoMessage(true, "Error al insertar el registro en base de datos", rule);
+        }
+        else if(error == 1)
+        {
+            message = new InfoMessage(true, "Ya existe en base de datos", rule);
         }
 
         return message;
     }
 
-    /**
-     * Crear un log de eliminación d euna regla en base de datos
-     * @param {RuleLog} rule Regla a marcar para eliminar
-     */
-    function deleteRule(rule)
+    function MongoGetRules(date){
+        if(date!=null)
+        {
+            MongoClient.connect(urlConnexio, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db(nomdb);
+                //Find the first document in the customers collection:
+                dbo.collection("rules").find({
+                    version: {"$gt":date}
+                }).toArray(function(err, result) {
+                if (err) throw er
+                console.log(result);
+
+                db.close();
+                return result
+                });
+            });
+        }
+    }
+
+    function MongoInsertRule(obj)
     {
-        var error = 0;
-        var message;
+        if(obj!=null)
+        {  
+            MongoClient.connect(urlConnexio, function(err, db) {
+            if (err){ 
+                return 2;
+                throw err;
+            }
+            var dbo = db.db(nomdb);
 
-        rule.Operation = "Eliminar";
+            dbo.collection("rules").findOne({
+                name : obj.Name,
+                desc : obj.Description
+            },function(err, result){
+            if (err) throw er
+            //console.log("result:  "+result);
+            if(result!=null || obj.Operation != "crear")
+            {
+                return 1;
+            }
+            else
+            {
+                var rule = { name: obj.Name, desc: obj.Description, path: obj.Path,port:obj.Port,operation:obj.Operation,inOut:obj.InOut,permission:obj.Permission,version:obj.Version,protocol:obj.Protocol,author:obj.Author}      
+                dbo.collection("rules").insertOne(rule, function(err, res) {
+                    if (err)
+                    {
+                        return 2;
+                        throw err;
+                    } 
+                    console.log("Insert Rule Correctament");
+                    db.close();
+                    return 0;
+                });
+            }
+            });
+                });
 
-        // Creamos un registro para eliminar la regla
-        error = MongoDeleteRule(rule);
-
-        if(error == 0)
-        {
-            message = new InfoMessage(false, "Todo correcto, se ha insertado la nueva regla de eliminación en base de datos", rule);
         }
-        else
-        {
-            message = new InfoMessage(true, "Error al insertar el registro de eliminación en base de datos", rule);
-        }
-
-        return message;
     }
-
-    function enableRule(rule)
-    {
-        var error = 0;
-        var message;
-
-        rule.Operation = "Habilitar";
-
-        // Creamos el registro para habiliatar una regla
-        error = MongoEnableRule(rule);
-
-        if(error == 0)
-        {
-            message = new InfoMessage(false, "Todo correcto, se ha habilitado la regla en base de datos", rule);
-        }
-        else
-        {
-            message = new InfoMessage(false, "Error, no se ha podido habilitar la regla en base de datos", rule);
-        }
-
-        return message;
-    }
-
-    function disableRule(rule)
-    {
-        var error = 0;
-        var message;
-
-        rule.Operation = "Deshabilitar";
-
-        error = MongoDisableRule(rule);
-
-        if(error == 0)
-        {
-            message = new InfoMessage(false, "Todo correcto, se ha deshabilitado la regla en base de datos", rule);
-        }
-        else
-        {
-            message = new InfoMessage(false, "Error, no se ha podido deshabilitar la regla en base de datos", rule);
-        }
-
-        return message;
-    }
-
-});
